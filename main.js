d61892bf1e4c653a78f3240805a6296ca3f43ec7
OBro1961 <66807297+mrbuilder1961@users.noreply.github.com>
2020-12-15 20:35:31 -0600

    Trimmed code

--- main.js
+++ main.js
@@ -1,33 +1,30 @@
-const {Client, MessageEmbed} = require('discord.js')
+const { Client, MessageEmbed } = require('discord.js')
 const bot = new Client()
 const util = require('minecraft-server-util')
 const prefix = '!'
-const token = 'your-discord-bot-token-here'
- 
-bot.on('ready', () =>{
+
+bot.on('ready', () => {
     console.log('Server Status Bot is online!')
 })
 
-bot.on('message', message =>{
- 
+bot.on('message', message => {
+
     let args = message.content.substring(prefix.length).split(/ +/)
     
     switch(args[0]){
         case 'stat':
- 
             if(!args[1]) return message.channel.send('Minecraft Server IP Address required.')
  
-            util.status(serverAlias(args))
+            util.status(aliases(args))
                 .then((response) => {
-                    console.log(response);
-                    const Embed = handleResponse(response)
-                    message.channel.send(Embed)
+                    message.channel.send(infoEmbed(response))
                 })
-                .catch((error) => {
+                .catch((error, undefined) => {
                     console.log(error);
                     if(error != null) return message.channel.send(`Failed to get server info, maybe you spelled it wrong? Try again later.\nIf this error persists, either the server is down or there\'s a bug in my code.\n\|\| ${error} \|\|`)
+                    else if(undefined == undefined) return message.channel.send(`Failed to get server info, maybe you spelled it wrong? Try again later.\nIf this error persists, either the server is down or there\'s a bug in my code.\n\|\| ${undefined} (That means the server is probably off.)\|\|`)
                     throw error;
-                });
+                });                               
         }
     }
 )
@@ -35,35 +32,32 @@ bot.on('message', message =>{
 function printPlayers(players) {
     if(players == null) return ''
     else {
-        let playerRename1 = players.map((player) => player.name).join(', ').replace(/§a|§b|§c|§d|§e|§f|§g|§k|§l|§m|§n|§o|§r|§1|§2|§3|§4|§5|§6|§7|§8|§9|Â/g, '');
-        let playerRename2 = playerRename1.replace(/_/g, '\_');
-        let playerRename3 = ' (' + playerRename2 + ')';
-        return playerRename3;
+        let samplePlayers = players.map((player) => player.name).join(', ').replace(/§a|§b|§c|§d|§e|§f|§g|§k|§l|§m|§n|§o|§r|§1|§2|§3|§4|§5|§6|§7|§8|§9|Â/g, '').replace(/_/g, '\\_');
+        let viewPlayers = ` (${samplePlayers})`;
+        return viewPlayers;
     }
 }
 
-function handleResponse(response) {
-    let desc = response.description.descriptionText
-    let betterDesc = desc.trim().replace(/§a|§b|§c|§d|§e|§f|§g|§k|§l|§m|§n|§o|§r|§1|§2|§3|§4|§5|§6|§7|§8|§9|Â|â|\x98|\x83/g, '')
-    let betterDesc2 = betterDesc1.replace(/\x98/g, ':snowman:')
-    let betterDesc3 = betterDesc2.replace(/\x83/g, ':snowman2:')
+function infoEmbed(response) {
+    let betterDesc = response.description.descriptionText.replace(/§a|§b|§c|§d|§e|§f|§g|§k|§l|§m|§n|§o|§r|§1|§2|§3|§4|§5|§6|§7|§8|§9|Â|â/g, '').replace(/\x98/g, ':snowman:').replace(/\x83/g, ':snowman2:').trim()
     const Embed = new MessageEmbed()
-    .setTitle('"'+ response.host + '" Server Info')
+    .setTitle(`'${response.host}' Server Info`)
     .setColor('#2a9c34')
     .addFields(
-        {name: 'IP Address', value: response.host + ' ( :' + response.port + ')'},
-        {name: 'MOTD', value: betterDesc3},
+        {name: 'IP Address', value: `${response.host} (:${response.port})`},
+        {name: 'MOTD', value: betterDesc},
         {name: 'Version', value: response.version},
-        {name: 'Players', value: response.onlinePlayers + ' / ' + response.maxPlayers + printPlayers(response.samplePlayers)},
-        {name: 'Other Debug Info', value: 'Protocol Version: ' + response.protocolVersion}
+        {name: 'Players', value: `${response.onlinePlayers} / ${response.maxPlayers} ${printPlayers(response.samplePlayers)}`},
+        {name: 'Protocol Version', value: response.protocolVersion}
     )
     return Embed;
 }
 
-function serverAlias(args) {
-    if(args[1] != 'hypixel') return args[1];
-    else(args[1] == 'hypixel')
-        return 'mc.hypixel.net';
+function aliases(args) {
+    if(args[1] == 'alias1' | args[1] == 'alias1.1' | args[1] == 'alias1.2') return 'alias1 IP address';
+    else if(args[1] == 'hypixel' | args[1] == 'Hypixel' | args[1] == 'hypix' | args[1] == 'hypixle') return 'hypixel.net';
+        else if(args[1] == 'alias3' | args[1] == 'alias3.1' | args[1] == 'alias3.2' | args[1] == 'alias3.3' ) return 'alias3 IP address';
+// Etc...
 }
 
-bot.login(token)
+bot.login('your-discord-bot-token-here')
